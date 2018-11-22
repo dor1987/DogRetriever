aaa6f3857bf7fd243c277197def7bd60d83dafa3
GuyCo5 <co.guycohen@gmail.com>
2018-11-22 11:28:26 +0200

    menu activity

--- app/src/main/java/dtg/dogretriever/MainActivity.java
+++ app/src/main/java/dtg/dogretriever/MainActivity.java
@@ -2,6 +2,7 @@ package dtg.dogretriever;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickScanner(View view) {
+    }
+
+    public void clickFindMyDog(View view) {
+    }
+
+    public void clickSettings(View view) {
+    }
+
+    public void clickProfile(View view) {
     }
 }
