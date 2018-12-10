f3d9086566d54f958180de1562b274fcb18c4df4
Tal Farbman <talush_f@hotmail.com>
2018-12-10 12:34:16 +0200

    Popup for algorithms fragment is added

--- app/src/main/java/dtg/dogretriever/Presenter/MainActivity.java
+++ app/src/main/java/dtg/dogretriever/Presenter/MainActivity.java
@@ -1,18 +1,42 @@
 package dtg.dogretriever.Presenter;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Transition;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.PopupWindow;
 
+
+import java.util.ArrayList;
+
+import dtg.dogretriever.Model.Dog;
 import dtg.dogretriever.R;
+import dtg.dogretriever.View.DogNamesAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PopupWindow popupWindow = null;
+    private int popupWidth ;
+    private int popupHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DisplayMetrics displayMetrics = this.getResources().getDisplayMetrics();
+        popupWidth = displayMetrics.widthPixels ;
+        popupHeight = displayMetrics.heightPixels ;
+
     }
 
     public void clickScanner(View view) {
@@ -20,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickFindMyDog(View view) {
+       createPopUpChooseDogName();
 
     }
 
@@ -35,4 +60,49 @@ public class MainActivity extends AppCompatActivity {
     public void clickAbout(View view) {
 
     }
+
+
+    private void createPopUpChooseDogName(){
+
+        LayoutInflater layoutInflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.choose_dog_popup, null);
+
+        ListView listView = layout.findViewById(R.id.popup_dog_name_list_view);
+        DogNamesAdapter dogNamesAdapter = new DogNamesAdapter(createDogsList(),this);
+        listView.setAdapter(dogNamesAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //navigate to algo fragment
+            }
+        });
+
+        popupWindow = new PopupWindow(this);
+        popupWindow.setContentView(layout);
+        popupWindow.setWindowLayoutMode(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        popupWindow.setHeight(1);
+        popupWindow.setWidth(1);
+        popupWindow.setFocusable(true);
+        popupWindow.showAtLocation(layout, Gravity.CENTER, 1, 1);
+
+
+
+
+
+    }
+
+
+    private ArrayList<Dog> createDogsList(){
+
+        ArrayList<Dog> dogs = new ArrayList<>();
+        dogs.add(new Dog("Luka"));
+        dogs.add(new Dog("Nala"));
+
+        return dogs;
+    }
+
+
 }
