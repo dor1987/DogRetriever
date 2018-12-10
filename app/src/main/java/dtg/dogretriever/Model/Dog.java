7cf68b8a2e2359f26cf716ecadfb8ebc8ac66ce5
Tal Farbman <talush_f@hotmail.com>
2018-12-10 12:35:56 +0200

    Popup for algorithms fragment is added

--- app/src/main/java/dtg/dogretriever/Model/Dog.java
+++ app/src/main/java/dtg/dogretriever/Model/Dog.java
@@ -8,7 +8,9 @@ public class Dog {
     private String size;
     private String notes;
 
-
+    public Dog(String name){ //delete after creating database
+        this.name = name;
+    }
     public Dog(int collarId, String name, String breed, String color, String size, String notes) {
         this.collarId = collarId;
         this.name = name;
