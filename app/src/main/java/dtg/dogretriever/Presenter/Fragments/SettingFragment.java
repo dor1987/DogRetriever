47085b66c3e207decb73be9acc0d50f76e55719e
Tal Farbman <talush_f@hotmail.com>
2019-05-31 09:38:04 +0300

    UI changes

--- app/src/main/java/dtg/dogretriever/Presenter/Fragments/SettingFragment.java
+++ app/src/main/java/dtg/dogretriever/Presenter/Fragments/SettingFragment.java
@@ -1,14 +1,28 @@
 package dtg.dogretriever.Presenter.Fragments;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import dtg.dogretriever.Model.FirebaseAdapter;
+import dtg.dogretriever.Presenter.MainActivity;
 import dtg.dogretriever.R;
+
+import static dtg.dogretriever.Presenter.MyMessagingService.SHARED_PREFS;
 
 
 public class SettingFragment extends PreferenceFragmentCompat{
 
-
+    private Preference logoutBtn;
+    private FirebaseAuth mAuth;
+    private FirebaseAdapter firebaseAdapter;
     public SettingFragment() {
         // Required empty public constructor
     }
@@ -16,6 +30,25 @@ public class SettingFragment extends PreferenceFragmentCompat{
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.preferences);
+        mAuth = FirebaseAuth.getInstance();
+        logoutBtn = findPreference(getString(R.string.log_out));
+
+        logoutBtn.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+//                mAuth.signOut();
+//                Intent intent = new Intent(getContext(), MainActivity.class);
+//                SharedPreferences sh = getContext().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+//
+//                if(sh != null) {
+//                    sh.edit().clear().apply();
+//                }
+//                startActivity(intent);
+
+                return true;
+            }
+        });
+
 
     }
 }
